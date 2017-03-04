5d1abae5cb5af27f3b2def727be6b6a42f849103
TheBeachMaster <otienoken@outlook.com>
2017-03-05 00:01:10 +0300

    Made Changes for Peer Connection

--- CodelabNpm/index.js
+++ CodelabNpm/index.js
@@ -5,13 +5,13 @@ var nodeStatic = require('node-static');
 var http = require('http');
 var socketIO = require('socket.io');
 
-var fileServer = new (nodeStatic.Server)();
-var app = http.createServer(function (req, res) {
+var fileServer = new(nodeStatic.Server)();
+var app = http.createServer(function(req, res) {
   fileServer.serve(req, res);
 }).listen(8080);
 
 var io = socketIO.listen(app);
-io.sockets.on('connection', function (socket) {
+io.sockets.on('connection', function(socket) {
 
   // convenience function to log server messages on the client
   function log() {
@@ -20,20 +20,18 @@ io.sockets.on('connection', function (socket) {
     socket.emit('log', array);
   }
 
-  //acts as a message relay
-
-  socket.on('message', function (message) {
+  socket.on('message', function(message) {
     log('Client said: ', message);
     // for a real app, would be room-only (not broadcast)
     socket.broadcast.emit('message', message);
   });
 
-  socket.on('create or join', function (room) {
+  socket.on('create or join', function(room) {
     log('Received request to create or join room ' + room);
 
     var numClients = io.sockets.sockets.length;
     log('Room ' + room + ' now has ' + numClients + ' client(s)');
-    // manages WebRTC video chat 'rooms':
+
     if (numClients === 1) {
       socket.join(room);
       log('Client ID ' + socket.id + ' created room ' + room);
@@ -50,15 +48,19 @@ io.sockets.on('connection', function (socket) {
     }
   });
 
-  socket.on('ipaddr', function () {
+  socket.on('ipaddr', function() {
     var ifaces = os.networkInterfaces();
     for (var dev in ifaces) {
-      ifaces[dev].forEach(function (details) {
+      ifaces[dev].forEach(function(details) {
         if (details.family === 'IPv4' && details.address !== '127.0.0.1') {
           socket.emit('ipaddr', details.address);
         }
       });
     }
+  });
+
+  socket.on('bye', function(){
+    console.log('received bye');
   });
 
 });
